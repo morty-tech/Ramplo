47639ba179ef6425afb2ebe83661aa189e6d74d9
Nora Apsel <nora@himorty.com>
2025-08-21 02:39:34 +0000

    will it work

--- api/health.ts
+++ api/health.ts
@@ -1,4 +1,3 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-export default (_req: VercelRequest, res: VercelResponse) => {
+export default (_req: any, res: any) => {
   res.status(200).json({ ok: true, runtime: "nodejs20.x" });
 };
